68fefe0d47d9feff7e03025fdbc534279fedd219
Jimmy A. Asong Jr <jimmya@swplumb.com>
2019-10-09 14:50:50 +0800

    create blade master and index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+@section('contents')
+    <header>
+        <span class="avatar"><img src="images/users/" alt="" /></span>
+        <h1>Ok!</h1>
+        <p>Nice!</p>
+    </header>
+@endsection
