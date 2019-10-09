875ab17a4e091f69e532e17672e403422f377ca6
Jimmy A. Asong Jr <jimmya@swplumb.com>
2019-10-09 14:58:32 +0800

    add assets (css, js and images)

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,29 +1,28 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-    <title>User Card - </title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-</head>
-<body class="is-preload">
-<div id="wrapper">
-    <section id="main">
-        @yield('contents')
-    </section>
-    <footer id="footer">
-        <ul class="copyright">
-            <li>&copy; Pictureworks</li>
-        </ul>
-    </footer>
-
-</div>
-<script>
-    if ('addEventListener' in window) {
-        window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
-        document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
-    }
-</script>
-</body>
+    <head>
+        <title>User Card - </title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+        <noscript><link rel="stylesheet" href="{{ asset('assets/css/noscript.css') }}" /></noscript>
+    </head>
+    <body class="is-preload">
+        <div id="wrapper">
+            <section id="main">
+                @yield('contents')
+            </section>
+            <footer id="footer">
+                <ul class="copyright">
+                    <li>&copy; Pictureworks</li>
+                </ul>
+            </footer>
+        </div>
+        <script>
+            if ('addEventListener' in window) {
+                window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+                document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+            }
+        </script>
+    </body>
 </html>
