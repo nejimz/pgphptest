d2548cbf3d65b8002bd915d4f2e4fcd7e97248b5
Jimmy A. Asong Jr <jimmya@swplumb.com>
2019-10-10 11:47:19 +0800

    post api validation and update comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\User;
+use App\Comments;
 
 class UserController extends Controller
 {
@@ -16,7 +17,7 @@ class UserController extends Controller
 	    	if (is_null($user)) {
 	    		abort(404);
 	    	}
-	    	#dd($user);
+	    	
 	    	$data = compact('user');
 
 	    	return view('index', $data);
@@ -27,7 +28,7 @@ class UserController extends Controller
 
     public function index_post(Request $request)
     {
-    	if (!$request->has('json')) {
+    	/*if (!$request->has('json')) {
     		return response()->json(['JSON is required!'], 422);
     	}
 
@@ -35,14 +36,16 @@ class UserController extends Controller
 
 		if (is_null($json)) {
 			return response()->json(['Invalid JSON!'], 422);
-		}
+		}*/
 
 		$request->validate([
-			'id' => 'required',
+			'id' => 'required|numeric|unique:users,id',
 			'password' => 'required|same:720DF6C2482218518FA20FDC52D4DED7ECC043AB',
 			'comments' => 'required'
 		]);
+		$input = $request->only('comments');
+		Comments::whereUserId($request->id)->update($input);
 
-		return response()->json(['Invalid JSON!'], 422);
+		return response()->json(['Ok!'], 200);
     }
 }
